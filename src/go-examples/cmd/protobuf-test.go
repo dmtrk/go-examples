5515340a9db2e8cabb83433ac11e8a6d8f6a8152
Dmitri Koroza <dmitrik@traiana.com>
2018-01-16 16:33:04 -0500

    init

--- src/go-examples/cmd/protobuf-test.go
+++ src/go-examples/cmd/protobuf-test.go
@@ -9,18 +9,25 @@ import (
 
 func main() {
 	fmt.Println("main()")
+	test1()
+	test2()
+	test3()
+}
+
+func test1()  {
+	fmt.Println("test1()")
 	headers := map[string]string {"rsc": "3711", "r":   "2138"}
-	//
-	msg := protobuf.NewBytesMessage(headers,"some data")
+	data := "some data"
+	msg := protobuf.NewBytesMessage(headers,data)
 	fmt.Println("msg:    ",msg)
 	//
-	data, err := proto.Marshal(msg)
+	buf, err := proto.Marshal(msg)
 	if err != nil {
 		log.Fatal("marshaling error: ", err)
 	}
 	//
 	newMsg := &protobuf.BytesMessage{}
-	err = proto.Unmarshal(data, newMsg)
+	err = proto.Unmarshal(buf, newMsg)
 	if err != nil {
 		log.Fatal("unmarshaling error: ", err)
 	}
@@ -29,4 +36,37 @@ func main() {
 	if string(msg.GetData()) != string(newMsg.GetData()) {
 		log.Fatalf("data mismatch %q != %q", msg.GetData(), newMsg.GetData())
 	}
+}
+
+func test2()  {
+	fmt.Println("test2()")
+	headers := map[string]string {"rsc": "3711", "r":   "2138"}
+	data := "some data"
+	//
+	msg := protobuf.NewBytesMessage(headers,data)
+	buf, err := protobuf.BytesMessageToBytes(msg)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	newMsg, err := protobuf.BytesMessageFromBytes(buf)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	fmt.Println("newMsg: ",newMsg)
+}
+func test3()  {
+	fmt.Println("test3()")
+	headers := map[string]string {"h1": "v1", "h2":   "v2"}
+	fields := map[string]string {"f1": "v11", "f2":   "v22"}
+	//
+	msg := protobuf.NewFieldsMessage(headers,fields)
+	buf, err := protobuf.FieldsMessageToBytes(msg)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	newMsg, err := protobuf.FieldsMessageFromBytes(buf)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	fmt.Println("newMsg: ",newMsg)
 }
