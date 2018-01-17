4fa296a1c3c49706f9ca0a6286afd52253729eb0
Dmitri Koroza <dmitrik@traiana.com>
2018-01-17 12:40:01 -0500

    HttpPublisher

--- src/go-examples/pkg/http/HttpPublisher.go
+++ src/go-examples/pkg/http/HttpPublisher.go
@@ -2,17 +2,73 @@ package http
 
 import (
 	"fmt"
-	"go-examples/pkg/protobuf"
+	"net/http"
+	"strings"
+	"io/ioutil"
 )
 
-type HttpPublisher struct  {
-
+type HttpPublisher struct {
+	client   *http.Client
+	Url      string
+	Username string
+	Password string
 }
 
-func (self *HttpPublisher) Publish(headers map[string]string, data string){
+func NewHttpPublisher(properties map[string]string) *HttpPublisher {
+	instance := new(HttpPublisher)
+	instance.client = &http.Client{}
+	instance.Url = GetString(properties, "http.url", "https://httpbin.org/post")
+	instance.Username = GetString(properties, "http.username", "")
+	instance.Password = GetString(properties, "http.password", "")
+	//
+	return instance
+}
+
+func (self *HttpPublisher) Post(headers map[string]string, data string) (error) {
+	return self.Do("POST", headers, data)
+}
+
+func (self *HttpPublisher) Get(headers map[string]string, data string) (error) {
+	return self.Do("GET", headers, data)
+}
+
+func (self *HttpPublisher) Do(method string, headers map[string]string, data string) (error) {
 	fmt.Println("Publish()")
-	msg := protobuf.NewBytesMessage(headers,data)
-	fmt.Println("msg: ",msg.String())
-	fmt.Println("msg.Headers: ",msg.Headers)
-	fmt.Println("msg.Data:    ",msg.Data)
+	var err error
+	req, err := http.NewRequest(method, self.Url, strings.NewReader(data))
+	if err != nil {
+		return err
+	}
+	for key, val := range headers {
+		fmt.Println("Publish() adding header ",key,val)
+		req.Header.Add(key, val)
+	}
+	if (len(self.Username) > 0 && len(self.Password) > 0) {
+		req.SetBasicAuth(self.Username, self.Password)
+	}
+	if len(req.Header.Get("Content-Type")) == 0 {
+		req.Header.Add("Content-Type", "text/plain")
+	}
+
+	// Do POST
+	resp, err := self.client.Do(req)
+	if err != nil {
+		return err
+	} else {
+		defer resp.Body.Close()
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Publish() response: ", string(body))
+	return err
+}
+
+func GetString(properties map[string]string, key string, defaultValue string) string {
+	value := properties[key]
+	if len(value) > 0 {
+		return strings.TrimSpace(value)
+	}
+	return defaultValue
 }
