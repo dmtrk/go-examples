4133eecefbc96f5abf680e9ca898d611ad2b40e5
Dmitri Koroza <dmitrik@traiana.com>
2018-01-18 18:51:53 -0500

    DialRabbit(url string)

--- src/go-examples/pkg/rabbit/util.go
+++ src/go-examples/pkg/rabbit/util.go
@@ -1,7 +1,20 @@
 package rabbit
 
+import "github.com/streadway/amqp"
+
 const (
 	RABBIT_DEFAULT_URL = "amqp://localhost"
 	RABBIT_DEFAULT_USERNAME = "guest"
 	RABBIT_DEFAULT_PASSWORD = "guest"
 )
+
+func DialRabbit(url string) (*amqp.Connection, *amqp.Channel, error) {
+	var err error
+	var conn *amqp.Connection
+	var ch *amqp.Channel
+	conn, err = amqp.Dial(url)
+	if err == nil {
+		ch, err = conn.Channel()
+	}
+	return conn, ch, err
+}
